43e05b9d376f09692ad840f0ec37eb9f1ac66b8b
Paul M Jones <pmjones@ciaweb.net>
2004-04-25 16:24:11 +0000

    initial add to PEAR
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@157090 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+    
+    // report all errors
+    error_reporting(E_ALL);
+    
+    // include the class file
+    require_once 'Contact_Vcard_Parse.php';
+    
+    // instantiate a parser object
+    $parse = new Contact_Vcard_Parse();
+    
+?>
