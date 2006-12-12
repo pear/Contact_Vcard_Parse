506f7f17c07f96dc2d6272498629be9032dbac47
Christian Weiske <cweiske@php.net>
2006-12-12 17:51:11 +0000

    QA bugfixing: * File is now at Contact/Vcard/Parse.php (still keeping old location for BC) * Fix bug #5988: does not work with apple adressbook exported vcard files * Using package.xml v2.0
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@224909 c90b9560-bf6c-de11-be94-00142212c4b1

--- test.php
+++ test.php
@@ -1,20 +1,18 @@
 <?php
-    
-    // report all errors
-    error_reporting(E_ALL);
-    
-    // include the class file
-    require_once 'Contact_Vcard_Parse.php';
-    
-    // instantiate a parser object
-    $parse = new Contact_Vcard_Parse();
-    
-    // parse it
-    $data = $parse->fromFile('test.vcf');
-    
-    // output results
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
-    
+// report all errors
+error_reporting(E_ALL);
+
+// include the class file
+require_once 'Contact/Vcard/Parse.php';
+
+// instantiate a parser object
+$parse = new Contact_Vcard_Parse();
+
+// parse it
+$data = $parse->fromFile('test.vcf');
+
+// output results
+echo '<pre>';
+print_r($data);
+echo '</pre>';
 ?>
