ef0720d62b83800ac5e4121657ff020f196dce16
Till Klampaeckel <till@php.net>
2007-11-19 01:45:28 +0000

    * cs fixes
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@246421 c90b9560-bf6c-de11-be94-00142212c4b1

--- Contact/Vcard/Parse.php
+++ Contact/Vcard/Parse.php
@@ -1,5 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
 /**
  * Parse vCard 2.1 and 3.0 text blocks.
  *
--- Contact_Vcard_Parse.php
+++ Contact_Vcard_Parse.php
@@ -1,6 +1,24 @@
 <?php
 /**
  * This file is kept for backwards compatibility reasons only
+ *
+ * Parse vCard 2.1 and 3.0 text blocks.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 2.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/2_02.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the world-wide-web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  File_Formats
+ * @package   Contact_Vcard_Parse
+ * @author    Paul M. Jones <pjones@ciaweb.net>
+ * @copyright 1997-2007 The PHP Group
+ * @license   http://www.php.net/license/2_02.txt  PHP License 2.0
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Contact_Vcard_Parse
  */
 require_once 'Contact/Vcard/Parse.php';
 ?>
