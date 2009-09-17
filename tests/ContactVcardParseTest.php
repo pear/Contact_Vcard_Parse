eba5d2e60176713e92272bd715caad81612207a8
Till Klampaeckel <till@php.net>
2009-09-17 19:39:02 +0000

    * fixed up tests (cs, some documentation, etc.)
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@288415 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/ContactVcardParseTest.php
+++ tests/ContactVcardParseTest.php
@@ -1,44 +1,102 @@
 <?php
-// Copyright (c) 2007 Contaxis Limited
+/**
+ * Parse vCard 2.1 and 3.0 text blocks.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 2.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/2_02.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the world-wide-web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  File_Formats
+ * @package   Contact_Vcard_Parse
+ * @author    Till Klampaeckel <till@php.net>
+ * @copyright Copyright (c) 2007 Contaxis Limited
+ * @license   http://www.php.net/license/2_02.txt  PHP License 2.0
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Contact_Vcard_Parse
+ */
 
+/**
+ * PHPUnit_Framework_TestCase
+ * @ignore
+ */
 require_once 'PHPUnit/Framework/TestCase.php';
 
-require_once "Contact_Vcard_Parse.php";
+/**
+ * Contact_Vcard_Parse
+ */
+require_once "Contact/Vcard/Parse.php";
 
+/**
+ * Tests for Contact_Vcard_Parse.
+ *
+ * @category  File_Formats
+ * @package   Contact_Vcard_Parse
+ * @author    Till Klampaeckel <till@php.net>
+ * @copyright Copyright (c) 2007 Contaxis Limited
+ * @license   http://www.php.net/license/2_02.txt  PHP License 2.0
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/Contact_Vcard_Parse
+ */
 class ContactVcardParseTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var string
+     * @see self::setUp()
+     */
+    protected $vcard;
 
-    public function setUp() {
-        $familyName = "FamilyName";
-        $givenName  = "GivenName";
+    /**
+     * @var Contact_Vcard_Parse
+     * @see self::setUp()
+     */
+    protected $parser;
+
+    /**
+     * setUp()
+     *
+     * Setup a vcard (in {@link self::$vcard}.
+     *
+     * @return void
+     * @uses   self::$vcard
+     * @uses   self::$parser
+     */
+    public function setUp()
+    {
+        $familyName      = "FamilyName";
+        $givenName       = "GivenName";
         $additionalNames = "Additional Names";
-        $prefix = "Prefix.";
-        $suffix = "Suffix";
-        $name = "A.N";
+        $prefix          = "Prefix.";
+        $suffix          = "Suffix";
+        $name            = "A.N";
 
-        $param1Name = "PARAM1";
+        $param1Name  = "PARAM1";
         $param1Value = "PARAMVALUE1";
-        $param2Name = "PARAM2";
+        $param2Name  = "PARAM2";
         $param2Value = "PARAMVALUE2";
         $param3Value = "PARAMVALUE3";
 
         $this->vcard = "BEGIN:VCARD\n\r" .
-                $name . 
-                    ";" . $param1Name . "=" . $param1Value .
-                    ";" . $param2Name . "=" . $param2Value .
-                    ";" . $param3Value .
-                    ":" .
-                    $familyName . ";" .
-                    $givenName . ";" .
-                    $additionalNames . ";" .
-                    $prefix . ";" .
-                    $suffix . "\n\r" .
-                "END:VCARD";
+            $name .
+            ";" . $param1Name . "=" . $param1Value .
+            ";" . $param2Name . "=" . $param2Value .
+            ";" . $param3Value .
+            ":" .
+            $familyName . ";" .
+            $givenName . ";" .
+            $additionalNames . ";" .
+            $prefix . ";" .
+            $suffix . "\n\r" .
+            "END:VCARD";
 
         $this->parser = new Contact_Vcard_Parse();
     }
 
-    public function testPropertyGroups() {
+    public function testPropertyGroups()
+    {
         list($ret) = $this->parser->fromText($this->vcard);
 
         list($data) = $ret["A.N"];
@@ -51,11 +109,11 @@ class ContactVcardParseTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("Suffix", $values[4][0]);
     }
 
-	public function testParameters() {
+	public function testParameters()
+    {
         list($ret) = $this->parser->fromText($this->vcard);
 
         list($data) = $ret["A.N"];
-
 
         $expected = array("PARAM1" => array("PARAMVALUE1"),
                           "PARAM2" => array("PARAMVALUE2"), 
@@ -64,4 +122,3 @@ class ContactVcardParseTest extends PHPUnit_Framework_TestCase
         $this->assertSame($expected, $data['param']);
     }
 }
-
