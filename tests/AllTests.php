c932aaf8bb099956f71551bb4c4beea003df18a3
Till Klampaeckel <till@php.net>
2009-09-21 13:41:20 +0000

    * added set_include_path() etc. to make it easier to run these form SVN
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@288526 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -6,6 +6,10 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once 'PHPUnit/Framework/TestSuite.php';
 
+// Uncomment if this is run from a SVN checkout
+//$inSvnDir = realpath(dirname(__FILE__) . '/../');
+//set_include_path($inSvnDir . PATH_SEPARATOR . get_include_path());
+
 require_once 'ContactVcardParseTest.php';
 
 class Contact_Vcard_Parse_AllTests {
