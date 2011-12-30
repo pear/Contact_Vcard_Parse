8d95c41f8b98a86dd79df88f7cf17d8dbf40600e
Daniel Convissor <danielc@php.net>
2011-12-30 23:26:59 +0000

    upgrade PHPUnit require statements (for PEAR QA Team).
    git-svn-id: http://svn.php.net/repository/pear/packages/Contact_Vcard_Parse/trunk@321594 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,14 +3,24 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Contact_Vcard_Parse_AllTests::main');
 }
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once 'PHPUnit/Framework/TestSuite.php';
+if ($fp = @fopen('PHPUnit/Autoload.php', 'r', true)) {
+    require_once 'PHPUnit/Autoload.php';
+} elseif ($fp = @fopen('PHPUnit/Framework.php', 'r', true)) {
+    require_once 'PHPUnit/Framework.php';
+    require_once 'PHPUnit/TextUI/TestRunner.php';
+} else {
+    die('skip could not find PHPUnit');
+}
+fclose($fp);
 
 // Uncomment if this is run from a SVN checkout
 //$inSvnDir = realpath(dirname(__FILE__) . '/../');
 //set_include_path($inSvnDir . PATH_SEPARATOR . get_include_path());
+//
+// Or, better yet, run the test suite from the directory above, like this:
+// php -d error_reporting=22527 tests/AllTests.php
 
-require_once 'ContactVcardParseTest.php';
+require_once dirname(__FILE__) . '/ContactVcardParseTest.php';
 
 class Contact_Vcard_Parse_AllTests {
 
